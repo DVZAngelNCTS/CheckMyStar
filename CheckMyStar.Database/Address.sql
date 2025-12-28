a94ac4ec7d7e3cc9d101299d90ef499f34cbb120
BOURDON ERIC <eric.bourdon+gdl@krys-group.com>
2025-12-28 14:50:31 +0100

    Ajout de la base de données

---
+++ CheckMyStar.Database/Address.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Identifier] INT NOT NULL PRIMARY KEY, 
+    [Number] VARCHAR(20) NOT NULL, 
+    [AddressLine] VARCHAR(100) NOT NULL, 
+    [City] VARCHAR(100) NOT NULL, 
+    [ZipCode] VARCHAR(20) NOT NULL, 
+    [Region] VARCHAR(50) NULL, 
+    [CountryIdentifier] INT NOT NULL, 
+    CONSTRAINT [FK_Address_Counrty] FOREIGN KEY ([CountryIdentifier]) REFERENCES [Country]([Identifier])
+)
---
+++ CheckMyStar.Database/User.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Identifier] INT NOT NULL IDENTITY, 
+    [Civility] INT NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [Society] VARCHAR(150) NULL, 
+    [Email] VARCHAR(100) NOT NULL UNIQUE, 
+    [Phone] VARCHAR(10) NULL, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [RoleIdentifier] INT NOT NULL, 
+    [AddressIdentifier] INT NOT NULL, 
+    CONSTRAINT [PK_User] PRIMARY KEY ([Identifier]), 
+    CONSTRAINT [FK_User_Address] FOREIGN KEY ([AddressIdentifier]) REFERENCES [Address]([Identifier]), 
+    CONSTRAINT [FK_User_Role] FOREIGN KEY ([RoleIdentifier]) REFERENCES [Role]([Identifier]) 
+)
