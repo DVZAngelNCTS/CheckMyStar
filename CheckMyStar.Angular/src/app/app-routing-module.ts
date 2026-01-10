6cdb24a70caac387e843d4e6594c566b5d7e5806
BOURDON ERIC <eric.bourdon+gdl@krys-group.com>
2026-01-10 12:56:06 +0100

    Ajout du routage des pages enfants

--- CheckMyStar.Angular/src/app/app-routing-module.ts
+++ CheckMyStar.Angular/src/app/app-routing-module.ts
@@ -4,12 +4,19 @@ import { FrontHomePageComponent } from './www/70_Modules/FrontOffice/Home/Home-p
 import { BackHomePageComponent } from './www/70_Modules/BackOffice/Home/Home-page.component';
 import { LoginPageComponent } from './www/70_Modules/FrontOffice/Login/Login-page.component';
 import { AuthenticateGuardian } from './www/80_Services/AuthenticateGuardian.service';
+import { RolePageComponent } from './www/70_Modules/BackOffice/Roles/Role-page.component';
+import { UserPageComponent } from './www/70_Modules/BackOffice/Users/User-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'fronthome', component: FrontHomePageComponent, canActivate: [AuthenticateGuardian] },
-  { path: 'backhome', component: BackHomePageComponent, canActivate: [AuthenticateGuardian] }
+  { path: 'backhome', component: BackHomePageComponent, canActivate: [AuthenticateGuardian],
+    children: [
+      { path : 'roles', component:  RolePageComponent},
+      { path : 'users', component:  UserPageComponent}
+    ]
+   },
 ];
 
 @NgModule({
